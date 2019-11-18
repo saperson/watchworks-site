ee0fd17dd5f6df026b922d2cbb9c6d2de4e40da3
BuildTools <unconfigured@null.spigotmc.org>
2019-11-18 09:25:29 -0800

    11/18 Commit 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,37 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
+'use strict'
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
 
 
 
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
+}
 
-
-
-
-
-
-
-
-
-
-
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);
 // End of Your Code . Don't delete that line below!!
 });

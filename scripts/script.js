2c0b0569734c1cfa65ffbae2502113bfce1987fa
BuildTools <unconfigured@null.spigotmc.org>
2019-12-07 14:41:35 -0800

    12/7 Commit 6

--- scripts/script.js
+++ scripts/script.js
@@ -3,35 +3,12 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
 'use strict'
 
+var aAudio = new Audio('clicksound.wav');
+     function myAudioFunction(letter) {
+         if(letter == 'a') {
+             aAudio.play();
+         } 
+     }
 
-function toggleIcon() {
-    $('.icon').on('click', function(){
-        $('.icon').toggleClass('active');
-        $('.menu-mobile').slideToggle(300);
-    })
-}
-
-function closeMobileMenu(){
-    $('.menu-mobile').on('click', 'a', function(){
-        $('.icon').trigger('click');
-    })
-}
-
-
-
-function desktopMenu() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
-            $('header.desktop').fadeIn(500);
-        } else {
-            $('header.desktop').fadeOut(500);
-        }
-    })
-}
-
-//when the page loads call toggleIcon;
-$(toggleIcon);
-$(closeMobileMenu);
-$(desktopMenu);
 // End of Your Code . Don't delete that line below!!
 });

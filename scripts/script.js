41a1533ef3c471ed92bab210476a1e73bdf7eeac
BuildTools <unconfigured@null.spigotmc.org>
2019-12-08 11:41:47 -0800

    12/8 Commit 1

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,5 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
 'use strict'
-
-var aAudio = new Audio('clicksound.wav');
-     function myAudioFunction(letter) {
-         if(letter == 'a') {
-             aAudio.play();
-         } 
-     }
-
 // End of Your Code . Don't delete that line below!!
 });

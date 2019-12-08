4ef9612638e509313ad1b7350f371da858a123b9
BuildTools <unconfigured@null.spigotmc.org>
2019-12-08 12:31:40 -0800

    12/8 Commit 3

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,5 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
 'use strict'
 
-var click = $("#click")[0];
-    $("a").click(function() {
-      click.play();
-    });
 // End of Your Code . Don't delete that line below!!
 });
